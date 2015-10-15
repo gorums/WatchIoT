9cca9e1368cc6dea779116a6ddd2eb054ae16f5d
gorums <acksecurity@hotmail.com>
2015-10-15 00:13:19 -0500

    Create plural DB name

--- db/migrate/20151014052020_create_project_request_to_servers.rb
+++ db/migrate/20151014052020_create_project_request_to_servers.rb
@@ -1,6 +1,6 @@
-class CreateProjectRequestsToServer < ActiveRecord::Migration
+class CreateProjectRequestToServers < ActiveRecord::Migration
   def change
-    create_table :project_requests_to_servers do |t|
+    create_table :project_request_to_servers do |t|
       t.string :url
       t.string :method        #GET or POST
       t.string :content_type
@@ -8,6 +8,6 @@ class CreateProjectRequestsToServer < ActiveRecord::Migration
       t.references :project_request
     end
 
-    add_index :project_requests_to_servers, :project_request_id
+    add_index :project_request_to_servers, :project_request_id
   end
 end
