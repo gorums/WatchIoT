e27a09a711b7322fad852a4c454b34eab380413d
gorums <acksecurity@hotmail.com>
2015-12-04 14:57:15 -0500

    Working on space logic

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,6 +1,9 @@
 class SpacesController < ApplicationController
   layout 'dashboard'
 
+  ##
+  # Get /:username/spaces
+  #
   def index
     owner_user = User.find_by_username(params[:username])  or not_found
 
@@ -8,11 +11,13 @@ class SpacesController < ApplicationController
       render 'general/spaces', layout: 'application'
       return
     end
-
     @space = Space.new
     @spaces = Space.where(user_id: owner_user.id).find_each
   end
 
+  ##
+  # Post /:username/spaces/create
+  #
   def create
     owner_user = User.find_by_username(params[:username])  or not_found
 
@@ -29,19 +34,39 @@ class SpacesController < ApplicationController
     end
   end
 
+  ##
+  # Get /:username/:spacename
+  #
   def show
     owner_user = User.find_by_username(params[:username])  or not_found
-    space = Space.find_by_name(params[:spacename])  or not_found
+    @space = Space.where(user_id: owner_user.id, name: params[:spacename]).first  or not_found
 
-    @spaces = Space.where(user_id: owner_user.id).find_each
+    if !is_auth? || current_user.username != owner_user.username
+      render 'general/spaces', layout: 'application'
+      return
+    end
+
+    @project = Project.new
   end
 
+  ##
+  # Get /:username/:spacename/setting
+  #
   def setting
+    owner_user = User.find_by_username(params[:username])  or not_found
+    @space = Space.where(user_id: owner_user.id, name: params[:spacename]).first  or not_found
 
+    if !is_auth? || current_user.username != owner_user.username
+      redirect_to '/'
+      return
+    end
   end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
+
+  ##
+  # Never trust parameters from the scary internet, only allow the white list through.
+  #
   def space_params
     params.require(:space).permit(:name, :description, :is_public)
   end
