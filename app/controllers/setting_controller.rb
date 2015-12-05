550aa5b89f68e17fcdd3f523c4402a41bcb0da44
gorums <acksecurity@hotmail.com>
2015-12-05 03:44:52 -0500

    Working on general setting logic

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -6,9 +6,9 @@ class SettingController < ApplicationController
   #
   def show
     redirect_to :root if !is_auth?
-    @user = User.find_by_username(params[:username])  or not_found
+    user = User.find_by_username(params[:username])  or not_found
 
-    if current_user.username != @user.username
+    if current_user.username != user.username
       redirect_to :root
     end
 
