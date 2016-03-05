c580f457dfd4f2c49b7de26f4d5488470922545c
gorums <acksecurity@hotmail.com>
2016-03-05 02:56:06 -0500

    Working over rspec  test

---
+++ spec/routing/setting_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'routes for setting', type: :routing do
+  it 'renders the subject' do
+
+  end
+end
---
+++ spec/routing/user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'routes for users', type: :routing do
+  it 'renders the subject' do
+
+  end
+end
