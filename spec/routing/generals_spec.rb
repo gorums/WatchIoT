578f30680ece2e83991a210b28071336e105fbea
gorums <acksecurity@hotmail.com>
2016-03-08 00:09:36 -0500

    Working over rspec  router user test

--- spec/routing/generals_spec.rb
+++ spec/routing/generals_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for generals', type: :routing do
-  it 'renders the subject' do
+  it 'routes home' do
+    expect(:get => '/').
+        to route_to(:controller => 'home', :action => 'index')
+  end
 
+  it 'routes contact us' do
+    expect(:post => '/contact').
+        to route_to(:controller => 'home', :action => 'contact')
+  end
+
+  it 'routes dashboard' do
+    expect(:get => '/gorums').
+        to route_to(:controller => 'dashboard', :action => 'show', :username => 'gorums')
   end
 end
