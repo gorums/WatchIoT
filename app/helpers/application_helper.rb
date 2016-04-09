4c88ae1a0bd4c6b66455ff8ff5ab1787a11d8649
gorums <acksecurity@hotmail.com>
2016-04-09 15:42:06 -0500

    Add predefined projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def embedded_svg(filename, options = {})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    if options[:title].present?
+      svg['title'] = options[:title]
+    end
+    doc.to_html.html_safe
+  end
+
 end
